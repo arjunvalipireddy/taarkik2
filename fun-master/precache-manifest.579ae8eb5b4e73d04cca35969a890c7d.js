19b0f792f11ef5dd0eca76da57b83a80021e6e95
arjunvalipireddy <53044723+arjunvalipireddy@users.noreply.github.com>
2025-04-03 17:31:15 -0400

    Add files via upload

---
+++ fun-master/precache-manifest.579ae8eb5b4e73d04cca35969a890c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0eec667349fdf5462a93c6716eb0455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef9b477f43fdb6a722f",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ec19b94d69cd0a6d4417",
+    "url": "/static/css/main.32ba7cd7.chunk.css"
+  },
+  {
+    "revision": "5ef9b477f43fdb6a722f",
+    "url": "/static/js/2.1f8f0819.chunk.js"
+  },
+  {
+    "revision": "ec19b94d69cd0a6d4417",
+    "url": "/static/js/main.4f95e6ff.chunk.js"
+  },
+  {
+    "revision": "1a64afd26cd642582716",
+    "url": "/static/js/runtime~main.42c3bd4f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
